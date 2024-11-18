03cf0637dc591a9b6d244e4193ad5e586ce463a3
mohammed faizan <faizan1999.in@gmail.com>
2024-11-19 01:38:32 +0530

    added a couple of more supporting functions

--- src/textnode.py
+++ src/textnode.py
@@ -1,6 +1,8 @@
 from enum import Enum
+from leafnode import LeafNode
 
 class TextType(Enum):
+    TEXT = "text"
     NORMAL = "normal"
     BOLD = "bold"
     ITALIC = "italic"
@@ -26,6 +28,42 @@ class TextNode:
         self.url == other.url)
 
     def __repr__(self):
-        return f"TextNode({self.text}, {self.text_type.value, {self.url}})"
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == TextType.LINK:
+        return LeafNode("a", (text_node.text, {"href": text_node.url}))
+    if text_node.text_type == TextType.IMAGE:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        
+    raise ValueError(f"Invalid text type: {text_node.text_type}")
 
 
+def split_nodes_delimiter(old_nodes, delimeter, text_type):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+        split_nodes = []
+        sections = old_node.text.split(delimeter)
+        if len(sections) % 2 == 0:
+            raise ValueError("Invalid Markdown")
+        for i in range(len(sections)):
+            if sections[i] == "":
+                continue
+            if i % 2 == 0:
+                split_nodes.append(TextNode(sections[i], TextType.TEXT))
+            else:
+                split_nodes.append(TextNode(sections[i]), text_type)
+
+        new_nodes.extend(split_nodes)
+    return new_nodes
